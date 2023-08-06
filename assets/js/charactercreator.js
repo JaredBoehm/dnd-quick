c8f7791eb3b1d2b28dc514b46f298761b43072ff
JaredBoehm <me@jaredboehm.com>
2023-08-05 19:51:34 -0600

    Initial work for printable character sheets

--- assets/js/charactercreator.js
+++ assets/js/charactercreator.js
@@ -1,8 +1,12 @@
-// TODO: Function that saves this to Local Storage
-let characterSheets = [] // Save this to Local Storage, the last characterSheet object in the array will be the last creation session (to save creation progress) 
+let characterSheets = JSON.parse(localStorage.getItem('characterSheets')) // grab character sheets from local storage, the current sheet will be appended to this on save
 
-// will need to keep track of the current index in characterSheets to update the current obj (can reopen and edit sheet?), if loading the creator again the last obj in the array the is last session
-// Character sheet object, push to characterSheets[] to save progress
+if (!characterSheets) { // if it doesn't exist create an empty character sheet array and save it to local storage
+    localStorage.setItem('characterSheets', '[]')
+    characterSheets = [] 
+} 
+
+let firstSelect = true // this is used in the get class so it doesn't auto reroll the dice on the first class select
+
 let characterSheet = {
     name: '', // update on input change
     race: '', // raceSelectElement.value
@@ -13,6 +17,7 @@ let characterSheet = {
     modifiers: {},  
     bonuses: [],
     hitpoints: 0,
+    armorClass: 0,
     skills: [], //an array of strings
 }
 
@@ -25,6 +30,8 @@ let raceDetailsElement = document.querySelector('#race-details')
 let classInfoElement = document.querySelector('#class-info')
 let classSkillElement = document.querySelector('#class-skills')
 let statInfoElement = document.querySelector('#stat-info')
+let saveCharacterButton = document.querySelector('#save-character')
+let viewSavedCharactersButton = document.querySelector('#view-saved-characters')
 
 
 // Functions
@@ -47,6 +54,7 @@ function raceSearch() {
             traits: data.traits,
         }
         characterSheet.raceInfo = raceInfo
+        characterSheet.race = data.name
 
         // start of drawing race details
         let detailsTitle = '<h1>Details</h1><hr class="border-slate-900">'
@@ -62,6 +70,7 @@ function raceSearch() {
             let bonusInfo = ('Bonus To: ' + bonusName + ' of ' + bonusAmt)
             raceDetailsElement.innerHTML += bonusInfo + '<br>'
 
+            bonusName = bonusName.toLowerCase()
             characterSheet.bonuses.push( { bonusName, bonusAmt } ) 
         }
         raceDetailsElement.innerHTML += '<br>' // new line
@@ -94,6 +103,12 @@ function raceSearch() {
             `
         }
         drawRaceInfo()
+
+
+        // if you are adjusting class/race after rolling your dice
+        if (characterSheet.race !== '' && characterSheet.class !== '') { // must select a race and a class before you can roll your stats
+            getStats()
+        }
     })
 }
 
@@ -119,13 +134,12 @@ function classSearch() {
 
         }
         characterSheet.classInfo = classInfo
+        characterSheet.class = data.name
 
         // start of drawing class details
-        classInfoElement.parentElement.classList.add('gap-10') // add spacing between details and skill options
         classInfoElement.innerHTML = '<h1>Details</h1><hr class="border-slate-900">'
-        classInfoElement.classList.add('p-4') // add some padding
-        classSkillElement.innerHTML = '<h1>Skill Options</h1><hr class="border-slate-900">'
-        classSkillElement.classList.add('p-4') // add some padding
+        classSkillElement.innerHTML = `<br><h1>Skill Options (Choose ${classInfo.skillsChoice})</h1><hr class="border-slate-900">`
+
 
         classInfoElement.innerHTML += 'Hit Die: ' + classInfo.hitdie + '<br>'
         classInfoElement.innerHTML += 'Skill Choices: ' + classInfo.skillsChoice + '<br><br>'
@@ -133,7 +147,16 @@ function classSearch() {
         // class skills
         for (let i = 0; i < classInfo.skills.length; i++) {
             let classSkills = classInfo.skills[i]
-            classSkillElement.innerHTML += classSkills.item.name.slice(7) + '<br>'
+
+            let skillName = classSkills.item.name.slice(7)
+            classSkillElement.innerHTML += 
+            `
+            <div class="py-4 md:py-px">
+                <input type="checkbox" name="${skillName}" value="${skillName}">
+                <label for="${skillName}">${skillName}</label>
+            </div>
+            `
+
             console.log(classSkills.item.name)
         }
 
@@ -161,12 +184,20 @@ function classSearch() {
             classInfoElement.innerHTML += 'Starting Equipment: ' + classEquipment.equipment.name + '<br>'
             console.log('Starting Equipment: ' + classEquipment.equipment.name)
         }
+
+
+        // if you are adjusting class/race after rolling your dice
+        if (characterSheet.race !== '' && characterSheet.class !== '' &&  firstSelect === false) { // must select a race and a class before you can roll your stats
+            getStats()
+        } else {
+            firstSelect = false
+        }
     })
 }
 
 function getStats() {
-    if (classSelectElement.value == 'select') { 
-        statInfoElement.innerHTML = 'You have to select a class!'
+    if (characterSheet.race == '' || characterSheet.class == '') { // must select a race and a class before you can roll your stats
+        statInfoElement.innerHTML = 'You have to select a race and a class!'
         return 
     }
 
@@ -176,11 +207,15 @@ function getStats() {
         dex: rollStat(),
         con: rollStat(),
         int: rollStat(),
-        wiz: rollStat(),
+        wis: rollStat(),
         cha: rollStat(),
     }
-    console.log(statsObject)
 
+    // add race bonuses
+    characterSheet.bonuses.forEach((bonus) => {
+        statsObject[bonus.bonusName] += bonus.bonusAmt
+    })
+ 
     // save statsObject to the global sheet object
     characterSheet.stats = statsObject
 
@@ -189,33 +224,34 @@ function getStats() {
         dex: Math.floor((statsObject.dex - 10) / 2),
         con: Math.floor((statsObject.con - 10) / 2),
         int: Math.floor((statsObject.int - 10) / 2),
-        wiz: Math.floor((statsObject.wiz - 10) / 2),
+        wis: Math.floor((statsObject.wis - 10) / 2),
         cha: Math.floor((statsObject.cha - 10) / 2),
     }
-    console.log(modifiersObject)
 
+    // save modifiersObject to the global sheet object
     characterSheet.modifiers = modifiersObject
 
+    // calculate armor class
     let armorClass = modifiersObject.dex + 10
-    console.log('AC: ' + armorClass)
+    // save it
+    characterSheet.armorClass = armorClass
     
+    // calculate hitdie
     let hitPoints = characterSheet.classInfo.hitdie + modifiersObject.con
-    console.log(typeof characterSheet.classInfo.hitdie)
-    console.log('HP: ' + hitPoints)
-
+    // save it
     characterSheet.hitpoints = hitPoints
 
     let drawStats = () => {
         statInfoElement.innerHTML = 
         `
         <ul class="p-2">
-            <li>Stats</li>
+            <li>Stats (With Race Bonuses)</li>
             <hr class="border-slate-900">
             <li>Strength: ${statsObject.str}</li>
             <li>Dexterity: ${statsObject.dex}</li>
             <li>Constitution: ${statsObject.con}</li>
             <li>Intelligence: ${statsObject.int}</li>
-            <li>Wisdom: ${statsObject.wiz}</li>
+            <li>Wisdom: ${statsObject.wis}</li>
             <li>Charisma: ${statsObject.cha}</li>
         </ul>
         <ul class="p-2">
@@ -225,7 +261,7 @@ function getStats() {
             <li>Dexterity: ${modifiersObject.dex}</li>
             <li>Constitution: ${modifiersObject.con}</li>
             <li>Intelligence: ${modifiersObject.int}</li>
-            <li>Wisdom: ${modifiersObject.wiz}</li>
+            <li>Wisdom: ${modifiersObject.wis}</li>
             <li>Charisma: ${modifiersObject.cha}</li>
         </ul>
         <ul class="p-2">
@@ -238,6 +274,66 @@ function getStats() {
     }
     drawStats()
 
+    // enable the save character button
+    saveCharacterButton.disabled = false
+
+}
+
+// save character object to charactersheets array, then push the array to localStorage
+function saveCharacter() {
+    if (saveCharacterButton.disabled === false) {
+
+        if (!characterName.value || characterName.value.trim() == '') { // if name is not provided
+            characterName.value = 'Unknown Wanderer'
+        }
+        characterSheet.name = characterName.value.trim() // characterName is defined in randomname.js
+    
+        let skills = classSkillElement.querySelectorAll(`input[type="checkbox"]`)
+        skills.forEach((skill) => {
+            if (skill.checked === true) {
+                characterSheet.skills.push(skill.value)
+            }
+        })
+    
+        characterSheets.push(characterSheet)
+        localStorage.setItem('characterSheets', JSON.stringify(characterSheets))
+
+        saveCharacterButton.textContent = 'Saved!' // display feedback
+        setTimeout(() => {
+            saveCharacterButton.textContent = 'Save Character'
+        }, 500)
+    }
+
+}
+
+// loading saved characters
+function viewSavedCharacters() {
+    document.body.innerHTML = 
+    `
+    <h1 class="text-xl pt-20">You have no saved characters!</h1>
+    <button onclick="location.assign('./')">New Character</button>
+    `
+    if (characterSheets.length > 0) {
+        document.body.innerHTML = `<h1 class="text-xl pt-20">Characters</h1>`
+        characterSheets.forEach((sheet, index) => {
+            let sheetData = JSON.stringify(sheet)
+            document.body.innerHTML += 
+            `
+            <section class="w-full flex justify-center">
+                <div class="p-4 bg-slate-950/50 rounded-md hover:cursor-pointer" onclick="redirect(${index})" >
+                    <h1 class="text-xl">${sheet.name} </h1>
+                    <p>${sheet.race} ${sheet.class}<p>
+                </div>
+            </section>
+            `
+        })
+        document.body.innerHTML += `<button class="pb-20" onclick="location.assign('./')">New Character</button>`
+    }
+
+}
+
+function redirect(index) {
+    location.assign('../sheetviewer/index.html?sheet=' + index)
 }
 
 
@@ -245,27 +341,5 @@ function getStats() {
 rollDice.addEventListener('click', () => getStats())
 raceSelectElement.addEventListener('change', () => raceSearch())
 classSelectElement.addEventListener('change', () => classSearch())
-
-
-
-
-// Generic Select Generator
-let testArray = ['class', 'class something', 'option 3', 'apple', 'yo']
-
-// TODO: can probably use this on any iterable object, not just arrays
-
-function createOptionList(optionArray) {
-    let wrapper = document.createElement('div')
-    wrapper.classList.add('select')
-
-    let select = document.createElement('select')
-
-    optionArray.forEach(optionString => {
-        let option = document.createElement('option')
-        option.innerText = optionString
-        select.append(option)
-    });
-
-    wrapper.append(select)
-    document.body.append(wrapper)
-}
+saveCharacterButton.addEventListener('click', () => saveCharacter())
+viewSavedCharactersButton.addEventListener('click', () => viewSavedCharacters())
